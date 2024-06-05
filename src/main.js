0a7eda853012efd939d3056e9e4c93d3e3a3526d
Wara Pacajes <wkpacajesm@gmail.com>
2024-06-04 23:54:10 -0400

    se avanzo con alertas

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const cardPepinillo = document.querySelector(".cardPepinillo");
+const cardEvil = document.querySelector(".cardEvil");
+
+cardPepinillo.addEventListener("click", function() {
+    alert("Pepinillo");
+});
+
+cardEvil.addEventListener("click", function() {
+    alert("Evil Morty");
+});
